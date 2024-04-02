ae86ea67f4b5f3d4c57ea5a0a279aee7351fbc67
Jorge Luis Boeira Bavaresco <jorge@jorgedebian.ifsul>
2024-04-02 08:51:20 -0300

    Tela de carregamento

--- src/componentes/telas/categoria/Categoria.jsx
+++ src/componentes/telas/categoria/Categoria.jsx
@@ -1,27 +1,31 @@
 import { useState, useEffect } from "react";
 import CategoriaContext from "./CategoriaContext";
-import { getCategoriasAPI, getCategoriaPorCodigoAPI, 
-    deleteCategoriaPorCodigoAPI, cadastrarCategoriaAPI } 
+import {
+    getCategoriasAPI, getCategoriaPorCodigoAPI,
+    deleteCategoriaPorCodigoAPI, cadastrarCategoriaAPI
+}
     from "../../../servicos/CategoriaServico";
 import Tabela from "./Tabela";
+import Carregando from "../../comuns/Carregando";
 
-function Categoria(){
+function Categoria() {
 
-    const [alerta, setAlerta] = useState({ status : "", message : ""});
+    const [alerta, setAlerta] = useState({ status: "", message: "" });
     const [listaObjetos, setListaObjetos] = useState([]);
     const [editar, setEditar] = useState(false);
-    const [objeto, setObjeto] = useState({codigo : "", nome : ""});
+    const [objeto, setObjeto] = useState({ codigo: "", nome: "" });
+    const [carregando, setCarregando] = useState(true);
 
     const novoObjeto = () => {
         setEditar(false);
-        setAlerta({ status : "", message : ""});
-        setObjeto({codigo : 0, nome : ""});        
+        setAlerta({ status: "", message: "" });
+        setObjeto({ codigo: 0, nome: "" });
     }
 
     const editarObjeto = async codigo => {
         setObjeto(await getCategoriaPorCodigoAPI(codigo));
         setEditar(true);
-        setAlerta({ status : "", message : ""});
+        setAlerta({ status: "", message: "" });
     }
 
     const acaoCadastrar = async e => {
@@ -29,12 +33,12 @@ function Categoria(){
         let metodo = editar ? "PUT" : "POST";
         try {
             let retornoAPI = await cadastrarCategoriaAPI(objeto, metodo);
-            setAlerta({status : retornoAPI.status, message : retornoAPI.message});
+            setAlerta({ status: retornoAPI.status, message: retornoAPI.message });
             setObjeto(retornoAPI.objeto);
-            if(!editar){
+            if (!editar) {
                 setEditar(true);
             }
-        } catch(err){
+        } catch (err) {
             console.log(err);
         }
         recuperaCategorias();
@@ -43,33 +47,37 @@ function Categoria(){
     const handleChange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        setObjeto({...objeto, [name] : value});
+        setObjeto({ ...objeto, [name]: value });
     }
 
     const recuperaCategorias = async () => {
+        setCarregando(true);
         setListaObjetos(await getCategoriasAPI());
+        setCarregando(false);
     }
 
     const remover = async codigo => {
         if (window.confirm('Deseja remover este objeto?')) {
             let retornoAPI = await deleteCategoriaPorCodigoAPI(codigo);
-            setAlerta({status : retornoAPI.status, message : retornoAPI.message});
+            setAlerta({ status: retornoAPI.status, message: retornoAPI.message });
             recuperaCategorias();
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         recuperaCategorias();
-    },[]);
+    }, []);
 
 
     return (
-        <CategoriaContext.Provider value={ {
-            alerta, listaObjetos, remover, 
+        <CategoriaContext.Provider value={{
+            alerta, listaObjetos, remover,
             objeto, editar, acaoCadastrar, handleChange, novoObjeto, editarObjeto
         }}>
-            <Tabela/>
-            
+            <Carregando carregando={carregando}>
+                <Tabela />
+            </Carregando>
+
         </CategoriaContext.Provider>
     )
 }
